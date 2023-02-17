4f168db2abfc055a6891b0a2781ed904dfd5d3b6
Takashi Sato <takashisato.develop@gmail.com>
2023-02-17 19:03:20 +0900

    chore: first commit

---
+++ src/lib.rs
@@ -0,0 +1,52 @@
+use swc_core::{
+    ecma::{
+        ast::{Program, CallExpr, Callee, Expr, MemberExpr, MemberProp, Lit, Str},
+        visit::{VisitMut, as_folder, FoldWith},
+        atoms::{JsWord}
+    },
+    common::{DUMMY_SP},
+    plugin::{plugin_transform, proxies::TransformPluginProgramMetadata}
+};
+
+pub struct TransformVisitor;
+
+impl VisitMut for TransformVisitor {
+    fn visit_mut_call_expr(&mut self, call: &mut CallExpr) {
+        if let Callee::Expr(expr) = &call.callee {
+            if let Expr::Member(MemberExpr { obj, prop, .. }) = &**expr {
+                if let MemberProp::Ident(ident) = prop {
+                    if ident.sym == *"log" {
+                        if let Expr::Ident(ident) = &**obj {
+                            if ident.sym == *"console" {
+                                call.args[0].expr = Box::new(Expr::Lit(Lit::Str(Str {
+                                    span: DUMMY_SP,
+                                    value: JsWord::from("阿部寛"),
+                                    raw: None,
+                                })));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#[plugin_transform]
+pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
+    program.fold_with(&mut as_folder(TransformVisitor))
+}
+
+#[cfg(test)]
+mod tests {
+    use swc_core::ecma::transforms::testing::test;
+    use super::*;
+
+    test!(
+        Default::default(),
+        |_| as_folder(TransformVisitor),
+        boo,
+        r#"console.log('foo');"#,
+        r#"console.log("阿部寛");"#
+    );    
+}
